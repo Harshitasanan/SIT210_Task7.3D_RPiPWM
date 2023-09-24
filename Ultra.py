e98e2061549f7f0fe52ec85cd9bc5b38e8460c73
Unique_h <132455967+Harshitasanan@users.noreply.github.com>
2023-09-24 15:53:49 +0530

    Update Ultra.py

--- Ultra.py
+++ Ultra.py
@@ -1,53 +1,60 @@
 import RPi.GPIO as GPIO
 import time
 
+# Set GPIO mode and pins
 GPIO.setmode(GPIO.BCM)
+TRIGGER_PIN = 14
+ECHO_PIN = 15
+LED_PIN = 18
 
-LED = 18
-GPIO_TRIGGER = 14
-GPIO_ECHO = 15
+# Set maximum distance and threshold
+MAX_DISTANCE = 10  # Maximum distance in centimeters
 
-MAX_DISTANCE = 10
+# Setup GPIO pins
+GPIO.setup(TRIGGER_PIN, GPIO.OUT)
+GPIO.setup(ECHO_PIN, GPIO.IN)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
-GPIO.setup(LED, GPIO.OUT)
-
-led = GPIO.PWM(LED, 100)
+# Create PWM object for LED
+led = GPIO.PWM(LED_PIN, 100)
 led.start(0)
 
-def distance():
-    GPIO.output(GPIO_TRIGGER, True)
+# Function to measure distance
+def measure_distance():
+    GPIO.output(TRIGGER_PIN, True)
     time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-    
-    StartTime = time.time()
-    StopTime = time.time()
-    
-    while GPIO.input(GPIO_ECHO) == 0:
-        StartTime = time.time()
-        
-    while GPIO.input(GPIO_ECHO) == 1:
-        StopTime = time.time()
-    
-    TimeElapsed = StopTime - StartTime
-    distance = (TimeElapsed * 34300) / 2  # Speed of sound is approximately 34300 cm/s
-    
+    GPIO.output(TRIGGER_PIN, False)
+
+    start_time = time.time()
+    stop_time = time.time()
+
+    while GPIO.input(ECHO_PIN) == 0:
+        start_time = time.time()
+
+    while GPIO.input(ECHO_PIN) == 1:
+        stop_time = time.time()
+
+    time_elapsed = stop_time - start_time
+    distance = (time_elapsed * 34300) / 2  # Speed of sound is approximately 34300 cm/s
+
     return distance
 
 try:
     while True:
-        dist = distance()
-        print("Measured Distance = %.1f cm" % dist)
+        dist = measure_distance()
+        print("Measured Distance = {:.1f} cm".format(dist))
 
-        if dist <= MAX_DISTANCE and dist > 0:
+        if 0 < dist <= MAX_DISTANCE:
             duty_cycle = (MAX_DISTANCE - dist) * 6.25
             led.ChangeDutyCycle(duty_cycle)
         else:
             led.ChangeDutyCycle(0)
+        
         time.sleep(0.1)  # Add a small delay to control the update rate
+
 except KeyboardInterrupt:
     print("Measurement stopped by user")
 
-led.stop()
-GPIO.cleanup()
+finally:
+    led.stop()
+    GPIO.cleanup()
